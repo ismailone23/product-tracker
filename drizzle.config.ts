b66950ab74ee8d6b11c3aa98f79fb436be67344f
ismailone23 <ismailhsanprsnl@gmail.com>
2024-08-17 23:36:33 +0600

    drizzle config change

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'drizzle-kit'
 export default defineConfig({
     dialect: 'postgresql',
     schema: "src/lib/schema/*",
-    out: "drizzle",
+    out: "./drizzle",
     dbCredentials: {
         url: env.DATABASE_URL
     },
